883b428cca13d771dabbde50b9d7165c31f22539
Ngoguey42 <ngoguey@student.42.fr>
2021-06-15 12:19:20 +0200

    lib_context: (recording step 2) Extract interactions with TEZOS_CONTEXT to env.ml

---
+++ src/lib_context/env.ml
@@ -0,0 +1,58 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2018-2021 Tarides <contact@tarides.com>                     *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type t = {
+  verbosity : [`Default | `Info | `Debug];
+  index_log_size : int option;
+  record_raw_trace : [`No | `Yes of string];
+}
+
+let default =
+  {verbosity = `Default; index_log_size = None; record_raw_trace = `No}
+
+let max_verbosity a b =
+  match (a, b) with
+  | (`Debug, _) | (_, `Debug) -> `Debug
+  | (`Info, _) | (_, `Info) -> `Info
+  | _ -> `Default
+
+let v =
+  match Unix.getenv "TEZOS_CONTEXT" with
+  | exception Not_found -> default
+  | v ->
+      List.fold_left
+        (fun acc -> function
+          | "v" | "verbose" ->
+              {acc with verbosity = max_verbosity acc.verbosity `Info}
+          | "vv" -> {acc with verbosity = `Debug}
+          | v -> (
+              match String.split '=' v with
+              | ["index-log-size"; n] ->
+                  {acc with index_log_size = int_of_string_opt n}
+              | ["action-trace-record-directory"; path] ->
+                  {acc with record_raw_trace = `Yes path}
+              | _ -> acc))
+        default
+        (String.split ',' v)
